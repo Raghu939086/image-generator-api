acf1fd3f803d50689296a805918b73325420ba17
Raghu939086 <gajamraghu991@gmail.com>
2026-02-05 21:18:13 +0530

    Clean up console logs in services.js
    Removed console logs for project ID, location, and credentials.

--- src/services/services.js
+++ src/services/services.js
@@ -1,14 +1,12 @@
 import { VertexAI } from "@google-cloud/vertexai";
 import { initGoogleAuth } from "../config/googleAuth.js";
 initGoogleAuth()
-console.log("PROJECT:", process.env.GOOGLE_PROJECT_ID);
-console.log("LOCATION:", process.env.GOOGLE_LOCATION);
+
 const vertexAI = new VertexAI({
   project: process.env.GOOGLE_PROJECT_ID,
   location: process.env.GOOGLE_LOCATION
 });
-console.log("Vertex AI initialized");
-console.log("GOOGLE_APPLICATION_CREDENTIALS:", process.env.GOOGLE_APPLICATION_CREDENTIALS);
+
 export const generateWithGemini = async ({ prompt, images }) => {
   const model = vertexAI.preview.getGenerativeModel({
     model: "gemini-2.5-flash-image"
