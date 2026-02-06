dec557960678d4e231ce23d1989835df7ae8606a
Raghu939086 <gajamraghu991@gmail.com>
2026-02-06 09:46:39 +0530

    Comment out server log message
    Comment out the server running log message.

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.use("/api/generate", generateRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(` Server running on http://localhost:${PORT}`);
+  //console.log(` Server running on http://localhost:${PORT}`);
 });
